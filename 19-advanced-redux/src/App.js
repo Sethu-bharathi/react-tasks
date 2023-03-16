0fbd9919897e261eeb23a181039a2cafd445d047
Sethu-bharathi <bharathisethu018@gmail.com>
2023-03-16 11:44:06 +0530

    Completed 19-redux

--- 19-advanced-redux/src/App.js
+++ 19-advanced-redux/src/App.js
@@ -2,49 +2,29 @@ import Cart from "./components/Cart/Cart";
 import Layout from "./components/Layout/Layout";
 import Products from "./components/Shop/Products";
 import { useSelector, useDispatch } from "react-redux";
-import { useState, useEffect } from "react";
+import { useEffect } from "react";
 import Notification from "./components/UI/Notification";
-import { uiActions } from "./store/ui-slice";
-
+import { fetchCartData, sendCartData } from "./store/cart-slice";
+let isInitial = true;
 function App() {
   const cartIsVisible = useSelector((state) => state.ui.cartIsVisible);
   const cart = useSelector((state) => state.cart);
   const dispatch = useDispatch();
   const notification = useSelector((state) => state.ui.notification);
-
   useEffect(() => {
-    const senDItems = async () => {
-      dispatch(
-        uiActions.setNotification({
-          status: "Pending",
-          title: "Sending",
-          message: "Data is being sent",
-        })
-      );
-      const response = await fetch(
-        "https://wind-turbine-power-predictor-default-rtdb.asia-southeast1.firebasedatabase.app/cart.json",
-        { method: "PUT", body: JSON.stringify(cart) }
-      );
-      if (!response.ok) {
-        throw new Error("Something went wrong");
-      }
-      dispatch(
-        uiActions.setNotification({
-          status: "success",
-          title: "Success",
-          message: "Data sent successfully",
-        })
-      );
-    };
-    senDItems().catch((error) => {
-      dispatch(
-        uiActions.setNotification({
-          status: "error",
-          title: "Error!",
-          message: "Data couldn't be sent",
-        })
-      );
-    });
+    if (isInitial) {
+      dispatch(fetchCartData());
+      return;
+    }
+  });
+  useEffect(() => {
+    if (isInitial) {
+      isInitial = false;
+      return;
+    }
+    if (cart.changed) {
+      dispatch(sendCartData(cart));
+    }
   }, [cart, dispatch]);
 
   return (
--- 19-advanced-redux/src/store/cart-slice.js
+++ 19-advanced-redux/src/store/cart-slice.js
@@ -1,4 +1,5 @@
 import { createSlice } from "@reduxjs/toolkit";
+import { uiActions } from "./ui-slice";
 
 const cartSlice = createSlice({
   name: "cart",
@@ -6,12 +7,19 @@ const cartSlice = createSlice({
     items: [],
     totalQuantity: 0,
     totalAmount: 0,
+    changed: false,
   },
   reducers: {
+    replaceCart: (state, action) => {
+      state.items = action.payload.items;
+      state.totalQuantity = action.payload.totalQuantity;
+      state.totalAmount = action.payload.totalAmount;
+    },
     addItem: (state, action) => {
       const newItem = action.payload;
+      state.changed = true;
       state.totalQuantity++;
-      state.totalAmount+=newItem.price
+      state.totalAmount += newItem.price;
       const existingItem = state.items.find((item) => item.id === newItem.id);
       if (!existingItem) {
         state.items.push({
@@ -27,20 +35,93 @@ const cartSlice = createSlice({
       }
     },
     removeItem: (state, action) => {
-        const id=action.payload;
-        const existingItem = state.items.find((item) => item.id === id);
-        if(existingItem.quantity===1){
-          state.items=state.items.filter(item=>item.id!==id);
-        }
-        else{
-          existingItem.quantity-=1;
-        }
-        state.totalQuantity--;
-        state.totalAmount-=existingItem.price
-
+      state.changed = true;
+      const id = action.payload;
+      const existingItem = state.items.find((item) => item.id === id);
+      if (existingItem.quantity === 1) {
+        state.items = state.items.filter((item) => item.id !== id);
+      } else {
+        existingItem.quantity -= 1;
+      }
+      state.totalQuantity--;
+      state.totalAmount -= existingItem.price;
     },
   },
 });
+
+export const sendCartData = (cart) => {
+  return async (dispatch) => {
+    dispatch(
+      dispatch(
+        uiActions.setNotification({
+          status: "Pending",
+          title: "Sending",
+          message: "Data is being sent",
+        })
+      )
+    );
+    const sendReq = async () => {
+      const response = await fetch(
+        "https://wind-turbine-power-predictor-default-rtdb.asia-southeast1.firebasedatabase.app/cart.json",
+        { method: "PUT", body: JSON.stringify(cart) }
+      );
+      if (!response.ok) {
+        throw new Error("Something went wrong");
+      }
+    };
+    try {
+      await sendReq();
+      dispatch(
+        uiActions.setNotification({
+          status: "success",
+          title: "Success",
+          message: "Data sent successfully",
+        })
+      );
+    } catch (e) {
+      dispatch(
+        uiActions.setNotification({
+          status: "error",
+          title: "Error!",
+          message: "Data couldn't be sent",
+        })
+      );
+    }
+  };
+};
+export const fetchCartData = () => {
+  return async (dispatch) => {
+    dispatch(
+      uiActions.setNotification({
+        status: "Pending",
+        title: "Fetching",
+        message: "Data is being fetched",
+      })
+    );
+    try {
+      const fetchData = await fetch(
+        "https://wind-turbine-power-predictor-default-rtdb.asia-southeast1.firebasedatabase.app/cart.json"
+      );
+      const data = await fetchData.json();
+      dispatch(
+        uiActions.setNotification({
+          status: "Success",
+          title: "Sucess",
+          message: "Data is fetched succesfully",
+        })
+      );
+      dispatch(cartActions.replaceCart({ ...data, items: data.items || [] }));
+    } catch (e) {
+      dispatch(
+        uiActions.setNotification({
+          status: "error",
+          title: "Error!",
+          message: "Data couldn't be sent",
+        })
+      );
+    }
+  };
+};
 
 export const cartActions = cartSlice.actions;
 export default cartSlice;
